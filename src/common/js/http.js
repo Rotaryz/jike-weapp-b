d2fcc57eae49fa035d2e2f62a608345c70836926
ben <xuedengheng@qq.com>
2018-01-16 18:59:45 +0800

    修改部分bug、优化部分配置

--- src/common/js/http.js
+++ src/common/js/http.js
@@ -20,6 +20,11 @@ export default class http {
     if (this.isSuccess(res)) {
       const result = res.data.data ? res.data.data : res.data
       return result
+    } else if (this.isLoseEfficacy(res)) {
+      wepy.navigateTo({
+        url: '/pages/logIn/logIn'
+      })
+      throw res.data
     } else {
       throw this.requestException(res)
     }
@@ -66,7 +71,17 @@ export default class http {
     const wxCode = res.statusCode
     // 微信请求错误
     if (wxCode === 200 || wxCode === 422) {
-      return true
+      const wxData = res.data
+      return wxData && wxData.error === 0
+    }
+    return false
+  }
+
+  static isLoseEfficacy(res) {
+    const wxCode = res.statusCode
+    if (wxCode === 200) {
+      const json = res.data
+      return json.status_code === 500 && json.error === 1 && json.message === '凭证已失效'
     }
     return false
   }
@@ -78,29 +93,15 @@ export default class http {
     const error = {}
     error.statusCode = res.statusCode
     const wxData = res.data
-    const serverData = wxData.data
-    if (serverData) {
-      error.serverCode = wxData.code
-      error.message = serverData.message
-      error.serverData = serverData
+    if (wxData) {
+      error.error = wxData.error
+      error.message = wxData.message
+      error.serverData = wxData
+      Tips.error(wxData.message)
+    } else {
+      Tips.loaded()
     }
     return error
-  }
-
-  /**
-   * 判断凭证
-   * @param msg
-   * @returns {Promise.<void>}
-   */
-  static async isLogin(msg) {
-    if (msg === '凭证已失效') {
-      await wepy.navigateTo({
-        url: '../logIn/logIn'
-      })
-      return 1
-    } else {
-      return 0
-    }
   }
 
   static get(url, data, loading = true) {
