ef72eb374d1f3ab4cbb567489d48c434b17013ba
philly <346692921@qq.com>
2018-03-10 16:27:30 +0800

    开通服务帐户绑定注册账户优化

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -32,11 +32,11 @@ const version = '/v2'
  * 测试环境
  * @type {{image: string, login: string, api: string}}
  */
-const URLS = {
-  image: 'https://img.jkweixin.net',
-  login: 'https://jwt.jkweixin.net',
-  api: 'https://backend-api.jkweixin.net' + version
-}
+// const URLS = {
+//   image: 'https://img.jkweixin.net',
+//   login: 'https://jwt.jkweixin.net',
+//   api: 'https://backend-api.jkweixin.net' + version
+// }
 // const URLS = {
 //   image: 'https://img.live.gytcrm.com',
 //   login: 'https://jwt.live.gytcrm.com',
@@ -47,11 +47,11 @@ const URLS = {
  * 生产环境
  * @type {{image: string, login: string, api: string}}
  */
-// const URIS = {
-//   image: 'https://img.jkweixin.com',
-//   login: 'https://jwt.jkweixin.com',
-//   api: 'https://backend-api.jkweixin.com' + version
-// }
+const URLS = {
+  image: 'https://img.jkweixin.com',
+  login: 'https://jwt.jkweixin.com',
+  api: 'https://backend-api.jkweixin.com' + version
+}
 
 class URIS {
   constructor() {
