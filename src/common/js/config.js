83aac4adcfce0dfda97cb4b1ef88a67c25571186
Rotaryz <1016994190@qq.com>
2018-03-22 19:08:54 +0800

    视频路劲

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -16,17 +16,17 @@
 //   B端api地址   https://backend-api.jkweixin.com
 //   C端api地址   https://wap-api.jkweixin.com
 
-const version = '/v2'
+const version = '/v1'
 
 /**
  * 研发环境
  * @type {{image: string, login: string, api: string}}
  */
-// const URLS = {
-//   image: 'https://img.jerryf.cn',
-//   login: 'https://dev.jike-jwt.jerryf.cn',
-//   api: 'https://dev.jike-backend-api.jerryf.cn' + version
-// }
+const URLS = {
+  image: 'https://img.jerryf.cn',
+  login: 'https://dev.jike-jwt.jerryf.cn',
+  api: 'https://dev.jike-backend-api.jerryf.cn' + version
+}
 
 /**
  * 测试环境
@@ -42,11 +42,11 @@ const version = '/v2'
  * 生产环境
  * @type {{image: string, login: string, api: string}}
  */
-const URLS = {
-  image: 'https://img.jkweixin.com',
-  login: 'https://jwt.jkweixin.com',
-  api: 'https://backend-api.jkweixin.com' + version
-}
+// const URLS = {
+//   image: 'https://img.jkweixin.com',
+//   login: 'https://jwt.jkweixin.com',
+//   api: 'https://backend-api.jkweixin.com' + version
+// }
 
 class URIS {
   constructor() {
