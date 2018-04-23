9e6a1720163dc2ab896ba6b3577c762dfdd66f20
eleven <wengtianliang@jkweixin.com>
2018-04-23 10:05:58 +0800

    分享活动UI

--- src/api/share-active.js
+++ src/api/share-active.js
@@ -8,11 +8,33 @@ import base from './base'
 export default class ShareMoney extends base {
   /**
    * 获取服务列表
-   * @param data
    * @returns {Promise.<*>}
    */
   static async getshareserver() {
     const url = `${this.baseUrl}/api/act-share-money/goods-list`
     return await this.get(url)
+  }
+  /**
+   * 获取服务列表
+   * @param 创建分享赚钱活动
+   * @returns {Promise.<*>}
+   */
+  static async createShareActive(data) {
+    const url = `${this.baseUrl}/api/share-money`
+    return await this.post(url, data)
+  }
+  /**
+   * 获取服务列表
+   * @param 获得分享赚钱活动
+   * @returns {Promise.<*>}
+   */
+  static async getShareActiveList(type, page, limit = 10) {
+    const url = `${this.baseUrl}/api/share-money`
+    let data = {
+      type,
+      page,
+      limit
+    }
+    return await this.get(url, data)
   }
 }
